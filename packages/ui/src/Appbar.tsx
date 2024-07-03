afca949cfb85d5b8921067c685e67b64f4f92819
Sanket Dadali <sanketdadali05@gmail.com>
2024-07-03 17:36:05 +0530

    UI pages created

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -14,7 +14,7 @@ export const Appbar = ({
     onSignin,
     onSignout
 }: AppbarProps) => {
-    return <div className="flex justify-between border-b px-4">
+    return <div className="flex justify-between border-b px-4 border-slate-400">
         <div className="text-lg flex flex-col justify-center">
             Venmo
         </div>
